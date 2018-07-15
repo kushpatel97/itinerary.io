516958924254781f4fc474c91efd0dc029c2ea85
Kush Patel <kushp447@gmail.com>
2018-07-15 14:51:11 -0400

    created post cards

--- react-client/src/components/modelComponents/post.jsx
+++ react-client/src/components/modelComponents/post.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
+import {veniceBG} from '../../images/veniceBG.jpg';
 
 class Post extends Component {
     render() {
         return (
-            <div className="card">
-                <h1 className="card-header">{this.props.location}</h1>
+        
+            <div className="card" style={styles.li}>
+                <img className="card-img-top" src="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiwiP754qHcAhVDhuAKHb7rDqEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.honeymoondreams.co.uk%2Fgreece-honeymoon-packages-thumbnail%2F&psig=AOvVaw3k3fzolMn24Qd_wi-TvDry&ust=1531767018870880" alt="Card image top"/>
                 <div className="card-body">
-                    <h2>{this.props.title}</h2>
-                    <h2>{this.props.author}</h2>
-                    <h2>{this.props.content}</h2>
-                    <h6 className="card-link">{this.props.upvotes}</h6>
-                    <h6 className="card-link">{this.props.downvotes}</h6>
+                <h3 className="card-title">{this.props.title}</h3>
+                <h4 className="card-subtitle">{this.props.location}</h4>
+                <p className="card-text">{this.props.content}</p>
+                <p className="card-text">{this.props.upvotes}</p>
+                <p className="card-text">{this.props.downvotes}</p>
                 </div>
             </div>
         );
@@ -20,7 +22,7 @@ class Post extends Component {
 
 const styles ={
     li: {
-        width: "80%"
+        width: "20rem"
     }
 }
 
